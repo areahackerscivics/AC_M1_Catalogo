d805feb05aa98d2df475f852ed8737b1c6ac5ef9
marymatt <marylin.mattos@gmail.com>
2018-01-13 12:46:42 +0100

    mejorando el código

--- catalogo.py
+++ catalogo.py
@@ -4,6 +4,7 @@
 import sys, os
 import datetime
 #from datetime import datetime
+from datetime import timedelta
 import urllib
 from pymongo import MongoClient#Libreria Mongodb
 parent_dir=os.getcwd()
@@ -30,6 +31,7 @@ def calculoCatalogo(filename,anyo,mes):
 
     #Se extrae el nombre de cada uno de los datasets
     datasets = coleccion.getElementsByTagName('dcat:dataset')
+    #Se extrae la fecha en la que se publicó el catálogo
     datafechaAct=coleccion.getElementsByTagName('dct:modified').item(0).firstChild.nodeValue
 
 
@@ -69,7 +71,6 @@ def calculoCatalogo(filename,anyo,mes):
 
             dicc[categoria] = dicc.get(categoria, 0) + 1
 
-
     result = []
     keys = dicc.keys()
     keys.sort()
@@ -89,7 +90,8 @@ def calculoCatalogo(filename,anyo,mes):
         print str(today),":Este registro ya existe "
 
 def getCatalogoBLL():
-    today=datetime.date.today()
+    DAY = timedelta(1)
+    today=datetime.date.today()-DAY
     anyo=today.strftime("%Y")
     mes=today.strftime("%m")
     """Devuelve el número de datasets por categoría.
@@ -103,60 +105,13 @@ def getCatalogoBLL():
 
     filename = 'catalogo_' + anyo + '_' + mes + '.rdf'
 
-
-    fecha = datetime.datetime.now()
-    anyoAct = fecha.strftime ("%Y")
-    mesAct =  fecha.strftime ("%m")
-
     if os.path.isfile("FILES/"+filename):
         return calculoCatalogo(filename,anyo,mes )
     else:
-        if ( anyo==anyoAct and mes==mesAct ):
-            #Si el año y el mes actuales son los mismos que los de la petición, nos descargamos el nuevo rdf de la web del ayuntamiento y realizamos el cálculo.
-            testfile = urllib.URLopener()
-            testfile.retrieve("http://gobiernoabierto.valencia.es/wp-content/themes/viavansi-ogov/proxyFile.php?url=http://gobiernoabierto.valencia.es/va/catalogo.rdf", 'FILES/'+filename)
+        #Si el año y el mes actuales son los mismos que los de la petición, nos descargamos el nuevo rdf de la web del ayuntamiento y realizamos el cálculo.
+        testfile = urllib.URLopener()
+        testfile.retrieve("http://gobiernoabierto.valencia.es/wp-content/themes/viavansi-ogov/proxyFile.php?url=http://gobiernoabierto.valencia.es/va/catalogo.rdf", 'FILES/'+filename)
+        return calculoCatalogo(filename,anyo,mes)
 
-            return calculoCatalogo(filename,anyo,mes)
 
-        else:
-            #Si no coinciden, nos estan pidiendo datos de catálogos pasados, los cuales no disponemos.
-            #Por lo que simplemente, se le devuelve una lista con valores a 0 para que el treeMap en la web
-            #muestre "No información disponible"
-            dicc = {
-                'ciencia tecnologia': 0,
-                'comercio': 0,
-                'cultura ocio': 0,
-                'demografia': 0,
-                'deporte': 0,
-                'economia': 0,
-                'educacion': 0,
-                'empleo': 0,
-                'energia': 0,
-                'hacienda': 0,
-                'industria': 0,
-                'legislacion justicia': 0,
-                'medio ambiente': 0,
-                'medio rural': 0,
-                'salud': 0,
-                'sector publico': 0,
-                'seguridad': 0,
-                'sociedad bienestar': 0,
-                'transporte': 0,
-                'turismo': 0,
-                'urbanismo infraestructuras': 0,
-                'vivienda': 0
-            }
-
-            result = []
-            keys = dicc.keys()
-            keys.sort()
-            for categoria in keys:
-                result.append( {'categoria': categoria, 'numDatasets': dicc[categoria]} )
-                catdb={
-                        "anyo":anyo,
-                        "mes":mes,
-                        "datos":result,
-                        "FechaUpdateCatalogo":datafechaAct
-                        }
-            return catdb
 getCatalogoBLL()
